9c15517ce15654c7af2d62cb9fe8a67cd11941f9
Stian Myklebostad <otard.code@proton.me>
2024-06-27 14:17:19 +0200

    feat(piantor_pro-qmk): Initial setup for piantor_pro qmk

---
+++ piantor_pro/qmk/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Tap-Hold settings
+#define TAPPING_TERM 150 // (default: 200ms)
+#define HOLD_ON_OTHER_KEY_PRESS
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY // Used to disable hold on other key press for home row mods
